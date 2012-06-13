8c7fc56cd15e885aafcf9300633d39f338375616
xazaj <zhuaijun@gmail.com>
2012-06-13 15:09:05 +0800

    Finish layout add routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,35 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  subject { page }
+
   describe "Home Page" do
-    it "should have the base title" do
-      visit root_path
-      page.should have_selector('title',text:"Ruby on Rails Sample App")
-    end
+    before { visit root_path }
 
-    it "should have the content 'sample app'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit root_path
-      page.should have_content('sample app')
-    end
+    it { should have_selector('h1', text:'Sample App') }
+    it { should have_selector('title', text:full_title('')) }
+  #  it { should_no have_selector text: '| Home' }
   end
-  
-  describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
-  end
-  
-  describe "About page" do
-    it "should have the content 'About Us' " do
-      visit about_path
-      page.should have_content('About Us')
-    end
-  end
-  
-  describe "Contact page" do
-    it "should have the contact 'Contact' " do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', text:'Contact')
-    end
 
-    it "should have title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title',text:"Ruby on Rails Sample App | Contact")
-    end
+  describe "Help Page" do
+    before { visit help_path}
+
+    it { should have_selector('h1', text:'Help') }
+    it { should have_selector('title', text:full_title('')) }
   end
+
+  describe "About Page" do
+    before { visit about_path}
+
+    it { should have_selector('h1', text:'About') }
+    it { should have_selector('title', text:full_title('')) }
+  end
+
+  describe "Contact Page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1', text:'Contact') }
+    it { should have_selector('title', text:full_title('')) }
+  end
+
 end
