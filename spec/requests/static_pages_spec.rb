9c476a3842168263f2323c99e88122ad1b09cbc3
xazaj <zhuaijun@gmail.com>
2012-06-13 14:21:11 +0800

    Message

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,27 +4,27 @@ describe "StaticPages" do
   
   describe "Home Page" do
     it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have have_selector('title',:text =>"Ruby on Rails Sample App")
+      visit root_path
+      page.should have_selector('title',text:"Ruby on Rails Sample App")
     end
 
     it "should have the content 'sample app'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('sample app')
     end
   end
   
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
   end
   
   describe "About page" do
     it "should have the content 'About Us' " do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
   end
@@ -32,7 +32,12 @@ describe "StaticPages" do
   describe "Contact page" do
     it "should have the contact 'Contact' " do
       visit '/static_pages/contact'
-      page.should have_selector('h1', :text=>'Contact')
+      page.should have_selector('h1', text:'Contact')
+    end
+
+    it "should have title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',text:"Ruby on Rails Sample App | Contact")
     end
   end
 end
