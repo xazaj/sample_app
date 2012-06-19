78a7767b3dbf48a1137965f00cb4500b9050f1a1
xazaj <zhuaijun@gmail.com>
2012-06-19 15:28:24 +0800

    modify edit action

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -66,6 +66,8 @@ describe "AuthenticationPages" do
 				describe "visiting the edit page" do
   					before { visit edit_user_path(user) }
   					it { should have_selector('title', text:'Sign in') }
+  					it { should_not have_link('Profile', href: user_path(user)) }
+  					it { should_not have_link('Settings', href: edit_user_path(user)) }
   				end
 
   				describe "submitting to the update action" do
@@ -76,6 +78,35 @@ describe "AuthenticationPages" do
   				describe "visiting the user index" do
   					before { visit users_path }
   					it { should have_selector('title', text: 'Sign in') }
+  				end
+  			end
+
+  			describe "when attempting to visit a protected page" do
+  				before do
+  					visit edit_user_path(user)
+  					fill_in "Email", with: user.email
+  					fill_in "Password", with: user.password
+  					click_button "Sign in"
+  				end
+
+  				describe "after signing in" do
+
+  					it "should render the desired protected page" do
+  						page.should have_selector('title', text: 'Edit user')
+  					end
+
+  					describe "when signing in again" do
+  						before do
+  							visit signin_path
+  							fill_in "Email", with: user.email
+  							fill_in "Password", with: user.password
+  							click_button "Sign in"
+  						end
+
+  						it "should render the default (profile) page" do
+  							page.should have_selector('title', text: user.name)
+  						end
+  					end
   				end
   			end
 		end
