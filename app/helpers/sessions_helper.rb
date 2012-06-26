2e817efe8cff44a9a14628e7e37f3b2e6e3aa2e9
xazaj <zhuaijun@gmail.com>
2012-06-27 01:57:22 +0800

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -34,4 +34,16 @@ module SessionsHelper
 	def store_location
 		session[:return_to] = request.fullpath
 	end
+
+	def current_user?(user)
+		user == current_user
+	end
+
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_path, notice: "Please sign in."
+		end
+	end
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -64,22 +64,22 @@ describe "AuthenticationPages" do
 			describe "in the Users controller" do
 
 				describe "visiting the edit page" do
-  					before { visit edit_user_path(user) }
-  					it { should have_selector('title', text:'Sign in') }
-  					it { should_not have_link('Profile', href: user_path(user)) }
-  					it { should_not have_link('Settings', href: edit_user_path(user)) }
-  				end
-
-  				describe "submitting to the update action" do
-  					before { put user_path(user) }
-  					specify { response.should redirect_to(signin_path) }
-  				end
-
-  				describe "visiting the user index" do
-  					before { visit users_path }
-  					it { should have_selector('title', text: 'Sign in') }
-  				end
+  				before { visit edit_user_path(user) }
+  				it { should have_selector('title', text:'Sign in') }
+  				it { should_not have_link('Profile', href: user_path(user)) }
+  				it { should_not have_link('Settings', href: edit_user_path(user)) }
   			end
+
+  			describe "submitting to the update action" do
+  				before { put user_path(user) }
+  				specify { response.should redirect_to(signin_path) }
+  			end
+
+  			describe "visiting the user index" do
+  				before { visit users_path }
+  				it { should have_selector('title', text: 'Sign in') }
+  			end
+  		end
 
   			describe "when attempting to visit a protected page" do
   				before do
@@ -109,7 +109,20 @@ describe "AuthenticationPages" do
   					end
   				end
   			end
-		end
+
+        describe "in the Microposts controller" do
+
+          describe "submitting to the create action" do
+            before { post microposts_path}
+            specify { response.should redirect_to(signin_path) }
+          end
+
+          describe "submitting to the destroy action" do
+            before { delete micropost_path(FactoryGirl.create(:micropost)) }
+            specify { response.should redirect_to(signin_path) }
+          end
+        end
+      end
 
   		describe "as wrong user" do
   			let(:user) { FactoryGirl.create(:user) }
@@ -132,8 +145,8 @@ describe "AuthenticationPages" do
   				before { delete user_path(user) }
   				specify { response.should redirect_to(root_path) }
   			end
-  		end
   	end
+  end
 end
 
 
